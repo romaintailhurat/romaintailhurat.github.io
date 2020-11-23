890c50cfa0725009096f00eaf50979900727fc13
romaintailhurat <romain.tailhurat@gmail.com>
2020-11-23 13:25:39 +0100

    yo, changing colors

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+const randomColor = () => {
+  const vals = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  const selection = ["#"];
+  for (let i = 0; i < 6; i++) {
+    const r = vals[Math.floor(Math.random() * vals.length) | 0];
+    selection.push(r);
+  }
+  return selection.join("");
+};
+
 const yospan = document.querySelector("span.yo");
+
 yospan.addEventListener("mouseover", (e) => {
   console.log("-= Y O =-");
 });
 
 yospan.addEventListener("click", (e) => {
   console.log("click");
+  yospan.style.color = randomColor();
 });
